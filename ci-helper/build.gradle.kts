5f932ac32e64e4d55aae33c22f82f68a81de8f7c
Him188 <Him188@mamoe.net>
2026-03-24 02:04:49 +0900

    Revert "Partially revert "Also update version code when build.""
    This reverts commit 830bfad02ad9acb983140f3d743938c02f5be03c.

--- ci-helper/build.gradle.kts
+++ ci-helper/build.gradle.kts
@@ -270,10 +270,11 @@ tasks.register("updateReleaseVersionNameFromGit") {
         val releaseVersion = ReleaseArtifactNames.fullVersionFromTag(ciTag.get())
         val releaseVersionCode = ReleaseArtifactNames.versionCodeFromTag(ciTag.get())
         val propertiesText = gradleProperties.readText()
-        println("New version name: $releaseVersion")
+        println("New version: $releaseVersion($releaseVersionCode)")
         gradleProperties.writeText(
-            propertiesText.replaceFirst(Regex("version.name=(.+)"), "version.name=$releaseVersion"),
-            // 不要更新 version.code, 这是为了让更新到测试版出 bug 的人可以回退到旧版
+            propertiesText
+                .replaceFirst(Regex("version.name=(.+)"), "version.name=$releaseVersion")
+                .replaceFirst(Regex("android.version.code=(.+)"), "android.version.code=$releaseVersionCode"),
         )
     }
 }
